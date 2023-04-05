0794703ecf904c6ffa281484b1274ed99de8cacd
Eduardo Tonatto <edtonatto@gmail.com>
2023-04-05 00:20:45 -0300

    :hammer: Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,15 @@
 export default {
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.module.ts',
+    '!<rootDir>/src/**/*.dataprovider.ts',
+    '!<rootDir>/src/**/*.resource.ts',
+    '!<rootDir>/src/**/*.restmodel.ts',
+    '!<rootDir>/src/**/*.model.ts',
+    '!<rootDir>/src/**/*.repository.ts',
+    '!<rootDir>/src/shared/**/*.ts',
+    '!<rootDir>/src/main.ts',
+  ],
   coveragePathIgnorePatterns: ['src/index.ts'],
   coverageDirectory: 'coverage',
   preset: 'ts-jest',
