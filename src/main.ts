8a4f804f01ba0376a18c1755e1468db9e1f6f9d0
Eduardo Tonatto <edtonatto@gmail.com>
2023-04-07 00:14:44 -0300

    :memo: Init swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Password Manager')
+    .setDescription(
+      'Password Manager is an API Rest to generate and store users passwords.',
+    )
+    .setVersion('0.0.1')
+    .addTag('User')
+    .addTag('Uptime')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('password-manager/docs', app, document);
+
   app.setGlobalPrefix('password-manager');
   await app.listen(3000);
 }
