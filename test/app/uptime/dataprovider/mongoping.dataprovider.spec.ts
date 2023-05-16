1392c8523e3db6390c096e6eb2267f0d7ad1c6ba
Eduardo Tonatto <edtonatto@gmail.com>
2023-05-16 13:20:32 -0300

    :white_check_mark: Added tests for mongo ping

--- test/app/uptime/dataprovider/mongoping.dataprovider.spec.ts
+++ test/app/uptime/dataprovider/mongoping.dataprovider.spec.ts
@@ -1,18 +1,38 @@
+import { getConnectionToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { mock } from 'jest-mock-extended';
 import { Connection } from 'mongoose';
 import { MongoPingProvider } from 'src/app/uptime/dataprovider/mongoping.provider';
 import { MongoPingDataProvider } from 'src/domain/uptime/dataprovider/mongoping.dataprovider';
 
 describe('MongoPingDataProvider', () => {
-  const connection = mock<Connection>();
   let mongoPingDataProvider: MongoPingDataProvider;
+  const mockConnection = {
+    db: {
+      command: jest.fn(),
+    }
+  }
 
   beforeEach(async () => {
-    mongoPingDataProvider = new MongoPingProvider(connection);
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        MongoPingProvider,
+        {
+          provide: getConnectionToken(),
+          useValue: mockConnection,
+        }
+      ]
+    }).compile();
+
+    mongoPingDataProvider = await moduleRef.resolve<MongoPingProvider>(MongoPingProvider);
   });
 
   it('should be defined', () => {
     expect(mongoPingDataProvider).toBeDefined();
+  });
+
+  it('should return "MongoDB is connected" if its ok', async () => {
+    mockConnection.db.command.mockResolvedValueOnce({ok: 1});
+    const result = await mongoPingDataProvider.ping();
+    expect(result).toEqual('MongoDB is connected');
   });
 });
