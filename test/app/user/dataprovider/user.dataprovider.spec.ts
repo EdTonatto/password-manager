04c67a68aa25e0fd840f7ae23a9080fd06ccecb2
Eduardo Tonatto <edtonatto@gmail.com>
2023-05-16 13:21:25 -0300

    :white_check_mark: Added tests for create user

--- test/app/user/dataprovider/user.dataprovider.spec.ts
+++ test/app/user/dataprovider/user.dataprovider.spec.ts
@@ -1,18 +1,49 @@
-import { mock } from 'jest-mock-extended';
+import { getModelToken } from '@nestjs/mongoose';
+import { Test } from '@nestjs/testing';
+import exp from 'constants';
 import { Model } from 'mongoose';
 import { User } from 'src/app/user/dataprovider/model/user.model';
 import { UserProvider } from 'src/app/user/dataprovider/user.provider';
+import { UserRestModel } from 'src/app/user/entrypoint/restmodel/user.restmodel';
 import { UserDataProvider } from 'src/domain/user/dataprovider/user.dataprovider';
 
 describe('UserDataProvider', () => {
-  const userModel = mock<Model<User>>();
   let userDataProvider: UserDataProvider;
+  const mockUserModel = {
+    create: jest.fn(),
+    save: jest.fn(),
+    fndByIdAndUpdate: jest.fn(),
+    findById: jest.fn(),
+    findOne: jest.fn(),
+  }
 
-  beforeEach(() => {
-    userDataProvider = new UserProvider(userModel);
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserProvider,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockUserModel,
+        }
+      ]
+    }).compile();
+
+    userDataProvider = await moduleRef.resolve<UserProvider>(UserProvider);
   });
 
   it('should be defined', () => {
     expect(userDataProvider).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const useRestModel: UserRestModel = {
+      name: 'test',
+      isAdmin: false,
+      email: 'test@test.test',
+      password: 'test',
+    }
+    mockUserModel.create.mockResolvedValueOnce(useRestModel);
+    const result = await userDataProvider.create(useRestModel);
+    expect(result).toEqual(useRestModel);
   });
 });
